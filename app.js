ac6f42b0f4d07926cc3668f3ce03e7c4c09b9fb9
yaron-cider <yaron.avital@cidersecurity.io>
2024-02-08 12:02:57 +0200

    added environment variables

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
+
 // CommonJS
 const {DefaultArtifactClient} = require('@actions/artifact')
 const artifact = new DefaultArtifactClient()
@@ -8,7 +11,25 @@ console.log("Starting upload...");
 (async () => {
     const {id, size} = await artifact.uploadArtifact(
         // name of the artifact
-        'my-artifact',
+        'super-linter-log',
+        // files to include (supports absolute and relative paths)
+        ['/usr/app/file.txt'],
+        // root directory to capture file paths from
+        '/usr/app',
+        {
+        // optional: how long to retain the artifact
+        // if unspecified, defaults to repository/org retention settings (the limit of this value)
+        retentionDays: 10
+        }
+    )
+})()
+
+
+
+(async () => {
+    const {id, size} = await artifact.uploadArtifact(
+        // name of the artifact
+        'super-linter-log',
         // files to include (supports absolute and relative paths)
         ['/usr/app/file.txt'],
         // root directory to capture file paths from
